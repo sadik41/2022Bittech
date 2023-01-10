5819c915056987b57eb8743afd8d5330cdbee686
sadik41 <sadikurrahman41@gmail.com>
2023-01-10 12:13:13 -0500

    added somthing in new class

--- TestNGFramework/src/main/java/db/Practice.java
+++ TestNGFramework/src/main/java/db/Practice.java
@@ -2,4 +2,15 @@ package db;
 
 public class Practice {
 
+	static int a =10;
+	public static void main(String[] args) {
+
+		 int a = 10;
+		 int b = 15;
+		 
+		 System.out.println(a+b);
+		
+	}
+	
 }
+
